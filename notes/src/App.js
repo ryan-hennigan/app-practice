e782ac48c72170051ffb17c49cdea84bf73fa2a2
ryanhennigan151 <ryan.hennigan@tamu.edu>
2019-04-23 21:24:44 -0500

    adding base notes app

--- notes/src/App.js
+++ notes/src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Note from './Components/Note';
+import uuid from 'uuid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Jumbotron ,Container,Row,Col,Button} from 'reactstrap';
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      notes : []
+    };
+
+  }
+
+  handleUpdate = (i,e) => {
+
+    const {notes} = this.state;
+
+    notes.map((n,j) => (n.id===i ? n.msg=e: n.msg ));
+
+    this.setState({
+      notes : notes
+    });
+  }
+
+addNote = () =>{
+  this.setState(previousState =>({
+    notes : [...previousState.notes, {id:uuid.v4(), msg:"new note"}]
+  }));
+}
+
+handleDelete = (i) => {
+  const {notes} = this.state;
+  const filtered = notes.filter(note => note.id !==i);
+  this.setState({
+    notes : filtered
+  });
+}
+
+  render() {
+    return(
+      <div className="App">
+        <Jumbotron>
+          <Container>
+            <h1 className="display-3">Note taking app</h1>
+
+          </Container>
+        </Jumbotron>
+        <Button onClick={this.addNote}>Add Note</Button>
+        {this.state.notes.map((note) =>(
+            <Note key={note.id} note={note} handleDelete={this.handleDelete} handleUpdate={this.handleUpdate}/>
+        ))}
+
+      </div>
+    );
+  }
+
 }
 
 export default App;
---
+++ notes/src/Components/Note.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import {Card,CardBody,Input,Button,Row,Col} from 'reactstrap';
+
+
+class Note extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      edit: false
+    };
+
+  }
+
+  handleClick = () =>{
+    this.setState({edit: !this.state.edit});
+  }
+
+  handleUpdate = (e) =>{
+    this.props.handleUpdate(this.props.note.id, e.target.value);
+  }
+
+  handleDelete = (e)=>{
+    this.props.handleDelete(this.props.note.id);
+  }
+
+
+  render(){
+    return(
+      <Card>
+        <Row>
+          <Col sm="10">
+            {!this.state.edit &&
+              <CardBody onClick={this.handleClick}>{this.props.note.msg}</CardBody>
+            }
+            {this.state.edit &&
+              <div>
+                <Input type="textarea" onBlur={this.handleClick} defaultValue={this.props.note.msg} onChange={this.handleUpdate}></Input>
+              </div>
+            }
+          </Col>
+          <Col sm="2">
+            <Button color="danger" onClick={this.handleDelete}>Delete</Button>
+          </Col>
+        </Row>
+      </Card>
+    );
+  }
+}
+
+export default Note;
